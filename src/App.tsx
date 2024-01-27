4bc0ca8b2d632ea3663beb71d5e359e47aafae53
kazirumi <tangorumi@gmail.com>
2024-01-28 00:29:56 +0600

    use axios to get put post

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button/Button";
@@ -8,7 +8,11 @@ import ExpandableText from "./components/ExpandableText/ExpandableText";
 import Form from "./components/Form/Form";
 import ExpenseTracker from "./components/ExpenseTracker";
 import ProductList from "./components/ProductList/ProductList";
-
+import axios, { CanceledError } from "axios";
+interface user {
+  id: number;
+  name: string;
+}
 function App() {
   // let [items, setItems] = useState([
   //   "Candy",
@@ -28,18 +32,104 @@ function App() {
   //   console.log("click");
   //   setShowAlert(!showAlert);
   // };
-const [category,SetCategory]= useState("");
+  // const [category,SetCategory]= useState("");
+
+  const [users, setUsers] = useState<user[]>([]);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+    axios
+      .get<user[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => setUsers(res.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => {
+      setUsers([]);
+      setError("");
+      controller.abort();
+    };
+  }, []);
+
+  let userDelete = (userToDelete: user) => {
+    // console.log(userToDelete.id);
+    const OriginalData = [...users];
+    setUsers(
+      users.filter((user) => {
+        return user.id !== userToDelete.id;
+      })
+    );
+
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + userToDelete.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers([...OriginalData]);
+      });
+  };
+
+  let userUpdate = (userToUpdate: user) => {
+    const OriginalData = [...users];
+    const updatedUser = { ...userToUpdate, name: userToUpdate.name + " updated" };
+    setUsers(
+      users.map((user) => {
+        if (user.id == updatedUser.id) return updatedUser;
+        return user;
+      })
+    );
+    axios
+      .put("https://jsonplaceholder.typicode.com/users/" + updatedUser.id,updatedUser)
+      .catch((err) => {
+        setError(err.message);
+        setUsers([...OriginalData]);
+      });
+  };
+
   return (
     <div>
-      
-      <select name="" onChange={(event)=>SetCategory(event.target.value)} id="" className="form-select">
+      {error && <p className="text-danger">{error}</p>}
+
+      <ul className="list-group">
+        {users.length > 0 &&
+          users.map((user, i) => (
+            <li
+              className="list-group-item d-flex justify-content-between"
+              key={user.id}
+            >
+              {user.name}
+              <div>
+                <button
+                  key={user.id}
+                  onClick={() => userUpdate(user)}
+                  className="btn btn-outline-secondary mx-2"
+                >
+                  Update
+                </button>
+                <button
+                  key={user.id}
+                  onClick={() => userDelete(user)}
+                  className="btn btn-outline-danger"
+                >
+                  Delete
+                </button>
+              </div>
+            </li>
+          ))}
+      </ul>
+
+      {/* <select name="" onChange={(event)=>SetCategory(event.target.value)} id="" className="form-select">
         <option value=""></option>
         <option value="Clothing">Clothing</option>
         <option value="HouseHold">Household</option>
 
       </select>
-      <ProductList category={category} />
-      
+      <ProductList category={category} /> */}
+
       {/* <ExpenseTracker /> */}
 
       {/* this was the last practices */}
